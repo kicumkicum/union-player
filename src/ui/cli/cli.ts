3976bbb6594e4e1c168be77870d5999389013111
kicumkicum <kicumkicum@gmail.com>
2021-03-27 13:38:47 +0300

    WIP: Listen key pressing

--- src/ui/cli/cli.ts
+++ src/ui/cli/cli.ts
@@ -26,13 +26,55 @@ const createCLI = (player: IStupidPlayer): void => {
     output: process.stdout,
   });
 
-  rl.on('line', async (line: string) => {
-    const [command, value] = line.split(' ');
-    console.log('line:', line)
+  readline.emitKeypressEvents(process.stdin);
+  process.stdin.setRawMode(true);
+  process.stdin.on('keypress', async (str, key) => {
+    // const [command, value] = line.split(' ');
+    // console.log('keypress')
+    const command = key.name;
+    const value = '1'
+    console.log(key)
+    // console.log('line:', line)
 
     switch (command) {
       case 'p':
         await p.togglePause();
+        process.stdout.moveCursor(0,-1);
+        process.stdout.clearLine(0)
+        // process.stdout.clearLine(0)
+        break;
+      case 's':
+      case 'n':
+      case 'right':
+        await p.stop();
+        process.stdout.clearLine(0)
+        process.stdout.moveCursor(-1,0);
+        break;
+      case 'm': {
+        const volume = p.getVolume()
+        await p.setVolume(volume > 0 ? 0 : 100);
+        break;
+      }
+      case 'v': {
+        const volume = parseInt(value, 10);
+        await p.setVolume(volume);
+        break;
+      }
+      case 'up':
+    }
+  });
+
+  return;
+  rl.on('line', async (line: string) => {
+    const [command, value] = line.split(' ');
+    // console.log('line:', line)
+
+    switch (command) {
+      case 'p':
+        await p.togglePause();
+        process.stdout.moveCursor(0,-1);
+        process.stdout.clearLine(0)
+        // process.stdout.clearLine(0)
         break;
       case 's':
       case 'n':
