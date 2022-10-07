ad07c06ab79be623ae4c11735954156008207018
kicumkicum <kicumkicum@gmail.com>
2022-10-07 10:20:35 +0300

    Fix album cover for chromecast

--- src/ui/chromecast/patch.ts
+++ src/ui/chromecast/patch.ts
@@ -1,6 +1,16 @@
 // @ts-ignore
 import {DefaultMediaReceiver} from 'castv2-client';
+import {Track} from "ym-api/dist/types";
 import {selectTrack} from "../../state/playlist-selectors";
+
+const getTrackSrc = (track: Track, size: number) => {
+  if (!track.coverUri) {
+    return ``;
+  }
+
+  // TODO: May be url is not correct
+  return `https://${track.coverUri.replace('%%', `${size}x${size}`)}`
+};
 
 // Workaround for change app_id
 DefaultMediaReceiver.APP_ID = '86E542A2';
@@ -15,6 +25,10 @@ DefaultMediaReceiver.prototype.load = function(media, options, callback) {
 
   media.metadata.artist = track.artists.map((it) => it.name).join(', ');
   media.metadata.albumName = track.albums[0].title;
+  media.metadata.title = track.title;
+  media.metadata.images = [{
+    url: getTrackSrc(track, 400),
+  }];
 
   this.media.load.apply(this.media, arguments);
 };
